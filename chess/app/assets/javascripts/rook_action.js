01127389a21bdfb9e396042fa24375c2db8804b7
Lawson Marlowe <lawsonmarlowe@gmail.com>
2016-05-25 13:52:25 -0400

    Add logic for Rook horiz movement, can't take own pieces.

--- chess/app/assets/javascripts/rook_action.js
+++ chess/app/assets/javascripts/rook_action.js
@@ -4,12 +4,23 @@ var rookAction = function() {
 		hoverClass: "ui-state-active",
 		
 		drop: function( event, ui ) {
-			if($(this).index() === startPos.index() && startPos.children().attr("class") === "white-rook white-piece chess-piece ui-draggable ui-draggable-handle"){
+
+			if($(this).index() === startPos.index() && startPos.children().attr("class") === "white-rook white-piece chess-piece ui-draggable ui-draggable-handle" && $(this).children().hasClass("white-piece") === false){
+			    playerTurn();
+			    return $( this )
+			    .html( ui.draggable );			    
+			}
+			else if($(this).parent()[0] === startPos.parent()[0] && startPos.children().attr("class") === "white-rook white-piece chess-piece ui-draggable ui-draggable-handle" && $(this).children().hasClass("white-piece") === false){
 			    playerTurn();
 			    return $( this )
 			    .html( ui.draggable );			    
 			};
-			if($(this).index() === startPos.index() && startPos.children().attr("class") === "black-rook black-piece chess-piece ui-draggable ui-draggable-handle"){
+			if($(this).index() === startPos.index() && startPos.children().attr("class") === "black-rook black-piece chess-piece ui-draggable ui-draggable-handle" && $(this).children().hasClass("black-piece") === false){
+			    playerTurn();
+			    return $( this )
+			    .html( ui.draggable );			    
+			}
+			else if($(this).parent()[0] === startPos.parent()[0] && startPos.children().attr("class") === "black-rook black-piece chess-piece ui-draggable ui-draggable-handle" && $(this).children().hasClass("black-piece") === false){
 			    playerTurn();
 			    return $( this )
 			    .html( ui.draggable );			    
