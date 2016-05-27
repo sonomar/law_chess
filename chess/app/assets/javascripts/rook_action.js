8aa667698250fbf353d9c77de23ee7bb8b2313d6
Lawson Marlowe <lawsonmarlowe@gmail.com>
2016-05-27 12:53:24 -0400

    Full vertical functionality for Rook works (cannot pass through other pieces)

--- chess/app/assets/javascripts/rook_action.js
+++ chess/app/assets/javascripts/rook_action.js
@@ -1,4 +1,6 @@
 var currentPos;
+var disableAboveArray = [];
+var disableBelowArray = [];
 
 var rookAction = function() {
 	$( ".white-square, .black-square" ).droppable({
@@ -8,45 +10,57 @@ var rookAction = function() {
 		// accept: ".valid",
 		
 		drop: function( event, ui ) {
-			var findPiece = function() {
+
+			var findPieceAbove = function() {
 			  var chessIndex = startPos.parent().index();
-              for(i = chessIndex; i > 0; i--) {
-              	if(startPos.parent().siblings().eq(i).children("span").eq(startPos.index()).children().hasClass("chess-piece")) {
-              	  for (neighbor = (i - 1); neighbor > 0; neighbor --) {
-                  	var disableAbove = startPos.parent().siblings().eq(neighbor).children().eq(startPos.index())
- 
-                  	 if(currentPos[0] === disableAbove[0]) {
-                  	 	startPos.children().removeClass("valid");
-                  	 	// console.log("hi")
-                  	 // };
-                  	 // $(currentPos).droppable({ disabled: true });
-                  	 // console.log($(disableAbove).droppable( "option", "disabled" ) === true);
-                     };
+			  disableAboveArray = [];
+              for(placement = chessIndex - 1; placement > 0; placement--) {
+              	if(startPos.parent().siblings().eq(placement).children("span").eq(startPos.index()).children().hasClass("chess-piece")) {
+              	  for (neighbor = (placement - 1); neighbor > 1; neighbor --) {
+                  	var disableAbove = startPos.parent().siblings().eq(neighbor).index();
+                    disableAboveArray.push(disableAbove);
                   };
+                 
+                  return disableAboveArray;
               	};
               };
 			};
-			currentPos = $(this);
-			if($(this).index() === startPos.index() && startPos.children().hasClass("white-rook") && $(this).children().hasClass("white-piece") === false){
-			    
-				// if!(startPos.children().hasClass("valid")) {
-				// 	startPos.children().addClass("valid");
-			 //    };
-			    // findPiece();
-			    playerTurn();
-			    return $( this )
-			    .html( ui.draggable );			    
-			}
-			else if($(this).parent()[0] === startPos.parent()[0] && startPos.children().hasClass("white-rook") && $(this).children().hasClass("white-piece") === false){
-			    
-			  //  if!(startPos.children().hasClass("valid")) {
-					// startPos.children().addClass("valid");
-			  //   };
-			    // findPiece();
-			    playerTurn();
-			    return $( this )
-			    .html( ui.draggable );			    
+
+			var findPieceBelow = function() {
+			  var chessIndex = startPos.parent().index();
+			  disableBelowArray = [];
+              for(placement = chessIndex; placement < 10; placement++) {
+              	if(startPos.parent().siblings().eq(placement).children("span").eq(startPos.index()).children().hasClass("chess-piece")) {
+              	  for (neighbor = (placement); neighbor < 10; neighbor ++) {
+                  	var disableBelow = startPos.parent().siblings().eq(neighbor).index();
+                    disableBelowArray.push(disableBelow);
+                  };
+                 
+                  return disableBelowArray;
+              	};
+              };
 			};
+
+			findPieceAbove();
+			findPieceBelow();
+			console.log(disableBelowArray);
+			currentPos = $(this);
+				if($(this).index() === startPos.index() && $(this).parent().index() != startPos.parent().siblings().eq(disableBelowArray[0]).index() && $(this).parent().index() != startPos.parent().siblings().eq(disableBelowArray[0] + 1).index() && $(this).parent().index() != startPos.parent().siblings().eq(disableBelowArray[0] + 2).index() && $(this).parent().index() != startPos.parent().siblings().eq(disableBelowArray[0] + 3).index() && $(this).parent().index() != startPos.parent().siblings().eq(disableBelowArray[0] + 4).index() && $(this).parent().index() != startPos.parent().siblings().eq(disableBelowArray[0] + 5).index() && $(this).parent().index() != startPos.parent().siblings().eq(disableBelowArray[0] + 6).index() && $(this).parent().index() != startPos.parent().siblings().eq(disableBelowArray[0] + 7).index() && $(this).parent().index() != startPos.parent().siblings().eq(disableAboveArray[disableAboveArray[0]]).index() && $(this).parent().index() != startPos.parent().siblings().eq(disableAboveArray[disableAboveArray[0]]).index() && $(this).parent().index() != startPos.parent().siblings().eq(disableAboveArray[disableAboveArray[0] - 1]).index() && $(this).parent().index() != startPos.parent().siblings().eq(disableAboveArray[disableAboveArray[0] - 2]).index() && $(this).parent().index() != startPos.parent().siblings().eq(disableAboveArray[disableAboveArray[0] - 3]).index() && $(this).parent().index() != startPos.parent().siblings().eq(disableAboveArray[disableAboveArray[0] - 4]).index() && $(this).parent().index() != startPos.parent().siblings().eq(disableAboveArray[disableAboveArray[0] - 5]).index() && $(this).parent().index() != startPos.parent().siblings().eq(disableAboveArray[disableAboveArray[0] - 6]).index() && $(this).parent().index() != startPos.parent().siblings().eq(disableAboveArray[disableAboveArray[0] - 7]).index() && startPos.children().hasClass("white-rook") && $(this).children().hasClass("white-piece") === false){
+				    debugger
+				    // findPiece();
+				    playerTurn();
+				    return $( this )
+				    .html( ui.draggable );			    
+				}
+			
+				else if($(this).parent()[0] === startPos.parent()[0] && startPos.children().hasClass("white-rook") && $(this).children().hasClass("white-piece") === false){
+			    
+			    // findPiece();
+			    playerTurn();
+			    return $( this )
+			    .html( ui.draggable );			    
+				};
+
 			if($(this).index() === startPos.index() && startPos.children().hasClass("black-rook") && $(this).children().hasClass("black-piece") === false){
 			    // findPiece();
 			    playerTurn();
