b96c5c9e2fdff77280a811b8b3d558b44b39f040
Lawson Marlowe <lawsonmarlowe@gmail.com>
2016-06-06 18:21:45 -0400

    Add optional piece transformation for Pawns reaching other side of the board. This completes original MVP.

--- chess/app/assets/javascripts/pawn_action.js
+++ chess/app/assets/javascripts/pawn_action.js
@@ -29,23 +29,83 @@ var pawnAction = function() {
 			}
 			else if($(this)[0] === startPos.parent().prev().children("span")[startPos.index()] && startPos.children().attr("class") === "white-pawn white-piece chess-piece ui-draggable ui-draggable-handle" && $(this).children()[0] === undefined && $(this).parent().index() === 0){
 			    playerTurn();
+			    var endSpot = $(this);
 			    startPos.empty();
-			    return $( this )
-			    .html( "<img class='white-queen white-piece chess-piece' src='http://www.iconsfind.com/wp-content/uploads/2015/11/20151106_563c235e690a0.png'>" );			    
+			    $(".white-select").css("display", "block");
+			    $(".white-rook-trade").click(function() {
+			    	$(".white-select").css("display", "none");
+			    	return endSpot
+			    .html( "<img class='white-rook white-piece chess-piece' src='http://www.iconsfind.com/wp-content/uploads/2015/11/20151106_563c2366cc7ae.png'>" );
+			    });
+			    $(".white-knight-trade").click(function() {
+			    	$(".white-select").css("display", "none");
+			    	return endSpot
+			    .html( "<img class='white-knight white-piece chess-piece' src='http://www.iconsfind.com/wp-content/uploads/2015/11/20151106_563c236365e2d.png'>" );
+			    });	
+			    $(".white-bishop-trade").click(function() {
+			    	$(".white-select").css("display", "none");
+			    	return endSpot
+			    .html( "<img class='white-bishop white-piece chess-piece' src='http://www.iconsfind.com/wp-content/uploads/2015/11/20151106_563c23557ae70.png'>" );
+			    });	
+			    $(".white-queen-trade").click(function() {
+			    	$(".white-select").css("display", "none");
+			    	return endSpot
+			    .html( "<img class='white-queen white-piece chess-piece' src='http://www.iconsfind.com/wp-content/uploads/2015/11/20151106_563c235e690a0.png'>" );
+			    });				    
 			}
 			else if($(this)[0] === startPos.parent().prev().children("span")[startPos.index()-1] && startPos.children().attr("class") === "white-pawn white-piece chess-piece ui-draggable ui-draggable-handle" && $(this).children()[0] != undefined && $(this).children().attr("class") != "white-pawn white-piece chess-piece ui-draggable ui-draggable-handle" && $(this).children().hasClass("black-king") === false && $(this).parent().index() === 0) {
 		        playerTurn();
+		        var endSpot = $(this);
 		        startPos.empty();
 		         $(".black-pieces-taken").append("<img class='taken-piece' src=" + $(this).children().attr("src") + ">");
-			    return $( this )
-			    .html( "<img class='white-queen white-piece chess-piece' src='http://www.iconsfind.com/wp-content/uploads/2015/11/20151106_563c235e690a0.png'>" );
+				    $(".white-select").css("display", "block");
+				    $(".white-rook-trade").click(function() {
+				    	$(".white-select").css("display", "none");
+				    	return endSpot
+				    .html( "<img class='white-rook white-piece chess-piece' src='http://www.iconsfind.com/wp-content/uploads/2015/11/20151106_563c2366cc7ae.png'>" );
+				    });
+				    $(".white-knight-trade").click(function() {
+				    	$(".white-select").css("display", "none");
+				    	return endSpot
+				    .html( "<img class='white-knight white-piece chess-piece' src='http://www.iconsfind.com/wp-content/uploads/2015/11/20151106_563c236365e2d.png'>" );
+				    });	
+				    $(".white-bishop-trade").click(function() {
+				    	$(".white-select").css("display", "none");
+				    	return endSpot
+				    .html( "<img class='white-bishop white-piece chess-piece' src='http://www.iconsfind.com/wp-content/uploads/2015/11/20151106_563c23557ae70.png'>" );
+				    });	
+				    $(".white-queen-trade").click(function() {
+				    	$(".white-select").css("display", "none");
+				    	return endSpot
+				    .html( "<img class='white-queen white-piece chess-piece' src='http://www.iconsfind.com/wp-content/uploads/2015/11/20151106_563c235e690a0.png'>" );
+				    });				    
 			}
 			else if($(this)[0] === startPos.parent().prev().children("span")[startPos.index()+1] && startPos.children().attr("class") === "white-pawn white-piece chess-piece ui-draggable ui-draggable-handle" && $(this).children()[0] != undefined && $(this).children().attr("class") != "white-pawn white-piece chess-piece ui-draggable ui-draggable-handle" && $(this).children().hasClass("black-king") === false && $(this).parent().index() === 0) {
 		        playerTurn();
+		        var endSpot = $(this);
 		        startPos.empty();
 		         $(".black-pieces-taken").append("<img class='taken-piece' src=" + $(this).children().attr("src") + ">");
-			    return $( this )
-			    .html( "<img class='white-queen white-piece chess-piece' src='http://www.iconsfind.com/wp-content/uploads/2015/11/20151106_563c235e690a0.png'>" );
+				    $(".white-select").css("display", "block");
+				    $(".white-rook-trade").click(function() {
+				    	$(".white-select").css("display", "none");
+				    	return endSpot
+				    .html( "<img class='white-rook white-piece chess-piece' src='http://www.iconsfind.com/wp-content/uploads/2015/11/20151106_563c2366cc7ae.png'>" );
+				    });
+				    $(".white-knight-trade").click(function() {
+				    	$(".white-select").css("display", "none");
+				    	return endSpot
+				    .html( "<img class='white-knight white-piece chess-piece' src='http://www.iconsfind.com/wp-content/uploads/2015/11/20151106_563c236365e2d.png'>" );
+				    });	
+				    $(".white-bishop-trade").click(function() {
+				    	$(".white-select").css("display", "none");
+				    	return endSpot
+				    .html( "<img class='white-bishop white-piece chess-piece' src='http://www.iconsfind.com/wp-content/uploads/2015/11/20151106_563c23557ae70.png'>" );
+				    });	
+				    $(".white-queen-trade").click(function() {
+				    	$(".white-select").css("display", "none");
+				    	return endSpot
+				    .html( "<img class='white-queen white-piece chess-piece' src='http://www.iconsfind.com/wp-content/uploads/2015/11/20151106_563c235e690a0.png'>" );
+				    });				    
 			}
 			else if($(this)[0] === startPos.parent().prev().prev().children("span")[startPos.index()] && startPos.parent().index() === 6 && startPos.children().attr("class") === "white-pawn white-piece chess-piece ui-draggable ui-draggable-handle" && $(this).children()[0] === undefined) {
 				 playerTurn();
@@ -72,23 +132,83 @@ var pawnAction = function() {
 			}
 			else if ($(this)[0] === startPos.parent().next().children("span")[startPos.index()] && startPos.children().attr("class") === "black-pawn black-piece chess-piece ui-draggable ui-draggable-handle" && $(this).children()[0] === undefined && $(this).parent().index() === 7) {
 			    playerTurn();
+			    var endSpot = $(this);
 			    startPos.empty();
-			    return $( this )
-			    .html( "<img class='black-queen black-piece chess-piece' src='http://www.iconsfind.com/wp-content/uploads/2015/11/20151106_563c235abcd0c.png'>" );
+			    $(".black-select").css("display", "block");
+			    $(".black-rook-trade").click(function() {
+			    	$(".black-select").css("display", "none");
+			    	return endSpot
+			    	.html( "<img class='black-rook black-piece chess-piece' src='http://www.iconsfind.com/wp-content/uploads/2015/11/20151106_563c23687e54b-210x210.png'>" );
+			    });
+			    $(".black-knight-trade").click(function() {
+			    	$(".black-select").css("display", "none");
+			    	return endSpot
+			    	.html( "<img class='black-knight black-piece chess-piece' src='http://www.iconsfind.com/wp-content/uploads/2015/11/20151106_563c23650ee18.png'>" );
+			    });
+			    $(".black-bishop-trade").click(function() {
+			    	$(".black-select").css("display", "none");
+			    	return endSpot
+			    	.html( "<img class='black-bishop black-piece chess-piece' src='http://www.iconsfind.com/wp-content/uploads/2015/11/20151106_563c235734a67.png'>" );
+			    });
+			    $(".black-queen-trade").click(function() {
+			    	$(".black-select").css("display", "none");
+			    	return endSpot
+			    	.html( "<img class='black-queen black-piece chess-piece' src='http://www.iconsfind.com/wp-content/uploads/2015/11/20151106_563c235abcd0c.png'>" );
+			    });
 			}
 			else if($(this)[0] === startPos.parent().next().children("span")[startPos.index()-1] && startPos.children().attr("class") === "black-pawn black-piece chess-piece ui-draggable ui-draggable-handle" && $(this).children()[0] != undefined && $(this).children().attr("class") != "black-pawn black-piece chess-piece ui-draggable ui-draggable-handle" && $(this).children().hasClass("white-king") === false && $(this).parent().index() === 7) {
 		        playerTurn();
+		        var endSpot = $(this);
 		        $(".white-pieces-taken").append("<img class='taken-piece' src=" + $(this).children().attr("src") + ">");
 		        startPos.empty();
-			    return $( this )
-			    .html( "<img class='black-queen black-piece chess-piece' src='http://www.iconsfind.com/wp-content/uploads/2015/11/20151106_563c235abcd0c.png'>" );
+			    	 $(".black-select").css("display", "block");
+			    $(".black-rook-trade").click(function() {
+			    	$(".black-select").css("display", "none");
+			    	return endSpot
+			    	.html( "<img class='black-rook black-piece chess-piece' src='http://www.iconsfind.com/wp-content/uploads/2015/11/20151106_563c23687e54b-210x210.png'>" );
+			    });
+			    $(".black-knight-trade").click(function() {
+			    	$(".black-select").css("display", "none");
+			    	return endSpot
+			    	.html( "<img class='black-knight black-piece chess-piece' src='http://www.iconsfind.com/wp-content/uploads/2015/11/20151106_563c23650ee18.png'>" );
+			    });
+			    $(".black-bishop-trade").click(function() {
+			    	$(".black-select").css("display", "none");
+			    	return endSpot
+			    	.html( "<img class='black-bishop black-piece chess-piece' src='http://www.iconsfind.com/wp-content/uploads/2015/11/20151106_563c235734a67.png'>" );
+			    });
+			    $(".black-queen-trade").click(function() {
+			    	$(".black-select").css("display", "none");
+			    	return endSpot
+			    	.html( "<img class='black-queen black-piece chess-piece' src='http://www.iconsfind.com/wp-content/uploads/2015/11/20151106_563c235abcd0c.png'>" );
+			    });
 			}
 			else if($(this)[0] === startPos.parent().next().children("span")[startPos.index()+1] && startPos.children().attr("class") === "black-pawn black-piece chess-piece ui-draggable ui-draggable-handle" && $(this).children()[0] != undefined && $(this).children().attr("class") != "black-pawn black-piece chess-piece ui-draggable ui-draggable-handle" && $(this).children().hasClass("white-king") === false && $(this).parent().index() === 7) {
 		        playerTurn();
+		        var endSpot = $(this);
 		        $(".white-pieces-taken").append("<img class='taken-piece' src=" + $(this).children().attr("src") + ">");
 		        startPos.empty()
-			    return $( this )
-			    .html( "<img class='black-queen black-piece chess-piece' src='http://www.iconsfind.com/wp-content/uploads/2015/11/20151106_563c235abcd0c.png'>" );
+			    $(".black-select").css("display", "block");
+			    $(".black-rook-trade").click(function() {
+			    	$(".black-select").css("display", "none");
+			    	return endSpot
+			    	.html( "<img class='black-rook black-piece chess-piece' src='http://www.iconsfind.com/wp-content/uploads/2015/11/20151106_563c23687e54b-210x210.png'>" );
+			    });
+			    $(".black-knight-trade").click(function() {
+			    	$(".black-select").css("display", "none");
+			    	return endSpot
+			    	.html( "<img class='black-knight black-piece chess-piece' src='http://www.iconsfind.com/wp-content/uploads/2015/11/20151106_563c23650ee18.png'>" );
+			    });
+			    $(".black-bishop-trade").click(function() {
+			    	$(".black-select").css("display", "none");
+			    	return endSpot
+			    	.html( "<img class='black-bishop black-piece chess-piece' src='http://www.iconsfind.com/wp-content/uploads/2015/11/20151106_563c235734a67.png'>" );
+			    });
+			    $(".black-queen-trade").click(function() {
+			    	$(".black-select").css("display", "none");
+			    	return endSpot
+			    	.html( "<img class='black-queen black-piece chess-piece' src='http://www.iconsfind.com/wp-content/uploads/2015/11/20151106_563c235abcd0c.png'>" );
+			    });
 			}
 			else if($(this)[0] === startPos.parent().next().next().children("span")[startPos.index()] && startPos.parent().index() === 1 && startPos.children().attr("class") === "black-pawn black-piece chess-piece ui-draggable ui-draggable-handle" && $(this).children()[0] === undefined) {
 				 playerTurn();
